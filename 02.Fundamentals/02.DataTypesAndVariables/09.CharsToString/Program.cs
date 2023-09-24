4fa84331941f0c279b1a604dcdbdf3a41a774afd
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-09-24 21:35:58 +0300

    Updated an exercise

--- 02.Fundamentals/02.DataTypesAndVariables/09.CharsToString/Program.cs
+++ 02.Fundamentals/02.DataTypesAndVariables/09.CharsToString/Program.cs
@@ -4,11 +4,7 @@
     {
         static void Main(string[] args)
         {
-            char ch1 = char.Parse(Console.ReadLine());
-            char ch2 = char.Parse(Console.ReadLine());
-            char ch3 = char.Parse(Console.ReadLine());
-
-            Console.WriteLine($"{ch1}{ch2}{ch3}");
+            Console.WriteLine(Console.ReadLine() + Console.ReadLine() + Console.ReadLine());
         }
     }
 }
