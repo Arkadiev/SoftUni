cb94da9c2aeb4927549c52d7acd9e24144e12d97
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-10-02 16:36:47 +0300

    Added new exercises

---
+++ 02.Fundamentals/03.Arrays/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{input[i]} ");
+            }
+
+        }
+    }
+}
---
+++ 02.Fundamentals/03.Arrays/07.EqualArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            bool areIdentical = true;
+
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                if (numbers1[i] != numbers2[i])
+                {
+                    areIdentical = false;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index"); break;
+                }
+                else
+                {
+                    sum += numbers1[i];
+                }
+            }
+
+            if (areIdentical == true)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+
+    }
+}
