0bb8cee35a6109437007daf7719b16ce19b7af0c
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-10-05 13:55:09 +0300

    Added new exercises

---
+++ 02.Fundamentals/03.ArraysExercise/08.MagicSum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int magicNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] + input[j] == magicNum)
+                    {
+                        Console.WriteLine($"{input[i]} {input[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
