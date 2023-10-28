01215048170504cd1d3c92c1ea962be07be1505c
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-10-28 13:09:13 +0300

    Added more exercises

---
+++ 02.Fundamentals/08.ObjectsAndClasses/01.RandomizeWords/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            Random rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(words.Length);
+
+                string word1 = words[pos1];
+                string word2 = words[pos2];
+
+                words[pos1] = word2;
+                words[pos2] = word1;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
---
+++ 02.Fundamentals/08.ObjectsAndClasses/03.Songs/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03.Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int songsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < songsCount; i++)
+            {
+                string[] arguments = Console.ReadLine().Split('_').ToArray();
+            }
+
+
+
+
+
+        }
+    }
+
+    class Song
+    {
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public double Time {  get; set; }
+    }
+
+
+}
