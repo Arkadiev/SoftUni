ffdee8ffb20ea342e25030c28b5188a14949bbbc
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-07 16:52:18 +0200

    Added new exercises

---
+++ 02.Fundamentals/09.AssociativeArrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> numberOccurrences = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!numberOccurrences.ContainsKey(numbers[i]))
+                {
+                    numberOccurrences[numbers[i]] = 1;
+                }
+                else
+                {
+                    numberOccurrences[numbers[i]]++;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> item in numberOccurrences)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ 02.Fundamentals/09.AssociativeArrays/02.OddOccurrences/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+        }
+    }
+}
