16f0d2ea6fa05c20613d260d1cded4a56b201025
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-07 17:11:32 +0200

    Added new exercises

--- 02.Fundamentals/09.AssociativeArrays/02.OddOccurrences/Program.cs
+++ 02.Fundamentals/09.AssociativeArrays/02.OddOccurrences/Program.cs
@@ -4,11 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] words = Console.ReadLine().Split().Select(x => x.ToLower()).ToArray();
 
+            var numberOccurrences = new Dictionary<string, int>();
 
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!numberOccurrences.ContainsKey(words[i]))
+                {
+                    numberOccurrences.Add(words[i], 1);
+                }
+                else
+                {
+                    numberOccurrences[words[i]]++;
+                }
+            }
 
-
+            foreach (KeyValuePair<string, int> kvp in numberOccurrences)
+            {
+                if (kvp.Value % 2 == 1)
+                {
+                    Console.Write($"{kvp.Key} ");
+                }
+            }
 
         }
     }
