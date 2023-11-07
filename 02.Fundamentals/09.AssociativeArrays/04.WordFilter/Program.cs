886b77c0b60814bb15ae16e9c36cc31879afccc3
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-07 18:12:52 +0200

    Added new exercises

---
+++ 02.Fundamentals/09.AssociativeArrays/04.WordFilter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToList()));
+        }
+    }
+}
