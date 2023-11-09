baa8cef7706af6e3014c4cec2838ac8a7bb5a70a
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-09 12:19:50 +0200

    Added new exercises

---
+++ 02.Fundamentals/09.AssociativeArraysExercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charOccurrences = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+
+                if (character == ' ')
+                {
+                    continue;
+                }
+
+                if (!charOccurrences.ContainsKey(character))
+                {
+                    charOccurrences.Add(character, 0);
+                }
+
+                charOccurrences[character]++;
+            }
+
+            foreach (var occurrence in charOccurrences)
+            {
+                Console.WriteLine($"{occurrence.Key} -> {occurrence.Value}");
+            }
+        }
+    }
+}
