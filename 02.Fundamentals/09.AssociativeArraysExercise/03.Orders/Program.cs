e14947a72a2f9f66245612a6c8d570973edc48b9
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-09 16:03:49 +0200

    Added new exercises

--- 02.Fundamentals/09.AssociativeArraysExercise/03.Orders/Program.cs
+++ 02.Fundamentals/09.AssociativeArraysExercise/03.Orders/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] arguments = input.Split();
+
+                string name = arguments[0];
+                decimal price = decimal.Parse(arguments[1]);
+                decimal quantity = decimal.Parse(arguments[2]);
+
+                Product product = new Product(name, price, quantity);
+
+                if (!products.ContainsKey(name))
+                {
+                    products.Add(name, product);
+                }
+                else
+                {
+                    products[name].Update(price, quantity);
+                }
+            }
+
+            foreach (var productPair in products)
+            {
+                Console.WriteLine(productPair.Value);
+            }
+        }
+    }
+
+    public class Product
+    {
+        public Product(string name, decimal price, decimal quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public decimal TotalPrice => Price * Quantity;
+
+        public void Update(decimal price, decimal quantity)
+        {
+            Price = price;
+            Quantity += quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} -> {TotalPrice:f2}";
         }
     }
 }
