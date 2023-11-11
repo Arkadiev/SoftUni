39922ca17069e0a52fb1c796e6bdb67b63f29679
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-11 13:37:26 +0200

    Added new exercises

---
+++ 02.Fundamentals/09.AssociativeArraysExercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, studentGrade);
+                }
+                else
+                {
+                    students[studentName] = (students[studentName] + studentGrade) / 2;
+                }
+            }
+
+            foreach (var studentPair in students)
+            {
+                if (studentPair.Value >= 4.50)
+                {
+                    Console.WriteLine($"{studentPair.Key} -> {studentPair.Value:f2}");
+                }
+            }
+            
+        }
+    }
+}
