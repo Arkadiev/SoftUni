b2ce77cbc1c10a94498abb1248a5c6426d1faa9b
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-13 11:50:47 +0200

    Added new exercises

--- 02.Fundamentals/10.TextProcessing/01.ReverseStrings/Program.cs
+++ 02.Fundamentals/10.TextProcessing/01.ReverseStrings/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string reversed = "";
+
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+                }
+
+                Console.WriteLine($"{input} = {reversed}");
+            }
         }
     }
 }
