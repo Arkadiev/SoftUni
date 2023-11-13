04e142758e117b6a0363bcd0d645f5e673c8a68d
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-13 11:58:11 +0200

    Added new exercises

---
+++ 02.Fundamentals/10.TextProcessing/04.TextFilter/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banwords = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+            
+            foreach (var banword in banwords)
+            {
+                if (text.Contains(banword))
+                {
+                    text = text.Replace(banword, new string('*', banword.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
