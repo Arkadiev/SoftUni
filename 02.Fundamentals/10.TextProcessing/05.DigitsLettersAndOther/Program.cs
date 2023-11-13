ca828b28fa5f06f1480b764338991fdc6b31310f
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-13 12:16:30 +0200

    Added new exercises

--- 02.Fundamentals/10.TextProcessing/05.DigitsLettersAndOther/Program.cs
+++ 02.Fundamentals/10.TextProcessing/05.DigitsLettersAndOther/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace _05.DigitsLettersAndOther
+﻿using System.Text;
+
+namespace _05.DigitsLettersAndOther
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder characters = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+
+                if (char.IsDigit(character))
+                {
+                    digits.Append(character);
+                }
+                else if (char.IsLetter(character))
+                {
+                    letters.Append(character);
+                }
+                else
+                {
+                    characters.Append(character);
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(characters);
         }
     }
 }
