2f265ba98634d4e3aff4ff313d27716a7d705b9f
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-16 17:11:55 +0200

    Added new exercises

--- 02.Fundamentals/10.TextProcessingExercise/04.CaesarCipher/Program.cs
+++ 02.Fundamentals/10.TextProcessingExercise/04.CaesarCipher/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _04.CaesarCipher
+﻿using System.Text;
+
+namespace _04.CaesarCipher
 {
     internal class Program
     {
@@ -6,7 +8,15 @@
         {
             string input = Console.ReadLine();
 
+            StringBuilder sb = new StringBuilder();
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                char original = input[i];
+                sb.Append((char)(original + 3));
+            }
+
+            Console.WriteLine(sb);
         }
     }
 }
