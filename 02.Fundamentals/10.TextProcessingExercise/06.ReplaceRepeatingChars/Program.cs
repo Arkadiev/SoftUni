67d96a716578796d7f362ac9d3afc8075234cd43
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-11-18 15:18:46 +0200

    Added new exercises

---
+++ 02.Fundamentals/10.TextProcessingExercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            StringBuilder resultBuilder = new StringBuilder();
+            resultBuilder.Append(input[0]);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i - 1])
+                {
+                    resultBuilder.Append(input[i]);
+                }
+
+            }
+
+            Console.WriteLine(resultBuilder);
+        }
+    }
+}
