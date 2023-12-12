92e5612502703c90a8e8da4dd7c7a346e40780b8
Arkadiev <georgi.arkadiev.96@gmail.com>
2023-12-12 14:44:19 +0200

    Added new exercises

---
+++ 02.Fundamentals/12.ExamPreparation/05.EmojiDetector/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _05.EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            BigInteger numbersSum = 1;
+            string numbersPattern = @"(?<number>\d)";
+            foreach (Match numberMatch in Regex.Matches(input, numbersPattern))
+            {
+                numbersSum *= int.Parse(numberMatch.Groups["number"].Value);
+            }
+
+            List<string> emojis = new List<string>();
+
+            BigInteger emojisCount = 0;
+            string emojisPattern = @"(::|\*\*)(?<word>[A-Z][a-z]{2,})(\1)";
+            foreach (Match emojiMatch in Regex.Matches(input, emojisPattern))
+            {
+                emojisCount += 1;
+                BigInteger lettersSum = 0;
+
+                string currentWord = emojiMatch.Groups["word"].Value;
+                for (int i = 0; i < currentWord.Length; i++)
+                {
+                    lettersSum += currentWord[i];
+                }
+
+                if (lettersSum > numbersSum)
+                {
+                    emojis.Add(emojiMatch.Value);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {numbersSum}");
+            Console.WriteLine($"{emojisCount} emojis found in the text. The cool ones are:");
+
+            foreach (string emoji in emojis)
+            {
+                Console.WriteLine(emoji);
+            }
+            
+        }
+    }
+}
