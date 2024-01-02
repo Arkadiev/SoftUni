12eaba4d19039ccf07c1b879cf6f023c5b0c8a1f
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-02 13:51:38 +0200

    Added new exercises

---
+++ 02.Fundamentals/02.MoreExercises/02.FromLeftToTheRight/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.FromLeftToTheRight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                long firstNumber = long.Parse(input[0]);
+                long secondNumber = long.Parse(input[1]);
+
+                if (firstNumber > secondNumber)
+                {
+                    long sum = 0;
+                    
+                    while (firstNumber != 0)
+                    {
+                        sum += firstNumber % 10;
+                        firstNumber /= 10;
+                    }
+
+                    Console.WriteLine(Math.Abs(sum));
+                }
+
+                else
+                {
+                    long sum = 0;
+
+                    while (secondNumber != 0)
+                    {
+                        sum += secondNumber % 10;
+                        secondNumber /= 10;
+                    }
+
+                    Console.WriteLine(Math.Abs(sum));
+                }
+            }
+        }
+    }
+}
