fa5a6db059c89fe3bd4ddaf46bdf558043283c68
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-02 14:03:00 +0200

    Added new exercises

---
+++ 02.Fundamentals/02.MoreExercises/03.FloatingEquality/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.FloatingEquality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal firstNumber = decimal.Parse(Console.ReadLine());
+            decimal secondNumber = decimal.Parse(Console.ReadLine());
+
+            decimal difference = Math.Abs(firstNumber - secondNumber);
+
+            if (firstNumber == secondNumber || difference < 0.000001m) { Console.WriteLine("True"); }
+            else { Console.WriteLine("False"); }
+        }
+    }
+}
---
+++ 02.Fundamentals/02.MoreExercises/04.RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.RefactoringPrimeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int number = 2; number <= input; number++)
+            {
+                bool isPrime = true;
+                for (int otherNumber = 2; otherNumber < number; otherNumber++)
+                {
+                    if (number % otherNumber == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                string prime = isPrime.ToString().ToLower();
+
+                Console.WriteLine($"{number} -> {prime}");
+            }
+        }
+    }
+}
