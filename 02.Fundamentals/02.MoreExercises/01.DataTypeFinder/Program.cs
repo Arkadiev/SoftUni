4a814af4f2b7dd87828f09c31a0f8e792772b961
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-02 13:16:49 +0200

    Added new exercises

---
+++ 02.Fundamentals/02.MoreExercises/01.DataTypeFinder/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _01.DataTypeFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int valueInt;
+            float valueFloat;
+            char valueChar;
+            bool valueBool;
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out valueInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out valueFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input,out valueChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input,out valueBool))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else // String
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
