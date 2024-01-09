bb4aa1ff2b0aa22c26710d368c89362041cbcd6b
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-09 17:54:00 +0200

    Added more exercises

---
+++ 02.Fundamentals/02.MoreExercises/05.DecryptingMessage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace _05.DecryptingMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+
+                int inputNew = (int)input + key;
+
+                Console.Write((char)inputNew);
+            }
+        }
+    }
+}
