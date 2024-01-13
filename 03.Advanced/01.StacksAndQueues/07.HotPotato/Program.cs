7723afe5577e12ff190098ef9815006dda3644a0
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-13 17:51:24 +0200

    Added new exercises

--- 03.Advanced/01.StacksAndQueues/07.HotPotato/Program.cs
+++ 03.Advanced/01.StacksAndQueues/07.HotPotato/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+
+            int n = int.Parse(Console.ReadLine());
+
+            int tosses = 1;
+
+            while (children.Count != 1)
+            {
+                string childWithPotato = children.Dequeue();
+
+                if (tosses == n)
+                {
+                    Console.WriteLine($"Removed {childWithPotato}");
+                    tosses = 0;
+                }
+                else
+                {
+                    children.Enqueue(childWithPotato);
+                }
+
+                tosses++;
+            }
+
+            Console.WriteLine($"Last is {children.Dequeue()}");
         }
     }
 }
