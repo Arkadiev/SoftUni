b956893e200f7ed588738e561db7b98ab374e0f9
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-13 19:06:14 +0200

    Added new exercises

--- 03.Advanced/01.StacksAndQueues/08.TrafficJam/Program.cs
+++ 03.Advanced/01.StacksAndQueues/08.TrafficJam/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            int totalPassed = 0;
+
+            Queue<string> cars = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    Console.WriteLine($"{totalPassed} cars passed the crossroads.");
+                    break;
+                }
+                else if (input == "green")
+                {
+                    if (n > cars.Count)
+                    {
+                        n = cars.Count;
+                    }
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                    }
+
+                    totalPassed += n;
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
         }
     }
 }
