a45943090b0062a018da057799b99fdf295bad46
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-13 17:36:21 +0200

    Added more exercises

--- 03.Advanced/01.StacksAndQueues/06.Supermarket/Program.cs
+++ 03.Advanced/01.StacksAndQueues/06.Supermarket/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            Queue<string> people = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Paid")
+                {
+                    while (people.Count > 0)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                else if (input == "End")
+                {
+                    Console.WriteLine($"{people.Count()} people remaining.");
+                    break;
+                }
+                else
+                {
+                    people.Enqueue(input);
+                }
+            }
         }
     }
 }
