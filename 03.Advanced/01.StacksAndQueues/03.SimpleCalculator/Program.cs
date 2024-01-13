0560e0de3601b247da4f162029a1a87ac2d26c6f
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-13 17:21:09 +0200

    Added new exercises

--- 03.Advanced/01.StacksAndQueues/03.SimpleCalculator/Program.cs
+++ 03.Advanced/01.StacksAndQueues/03.SimpleCalculator/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            Stack<string> stack = new Stack<string>(Console.ReadLine().Split().Reverse().ToArray());
+
+            int sum = int.Parse(stack.Pop());
+
+            while (stack.Count > 0)
+            {
+                char currentChar = char.Parse(stack.Pop());
+
+                if (currentChar == '+')
+                {
+                    int currentNumber = int.Parse(stack.Pop());
+                    sum += currentNumber;
+                }
+                else if (currentChar == '-')
+                {
+                    int currentNumber = int.Parse(stack.Pop());
+                    sum -= currentNumber;
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
