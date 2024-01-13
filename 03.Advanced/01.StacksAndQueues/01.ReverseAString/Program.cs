a77cdc9b76146c123684aab5c9f779a087586cec
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-13 12:00:26 +0200

    Added new exercises

---
+++ 03.Advanced/01.StacksAndQueues/01.ReverseAString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> word = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                word.Push(input[i]);
+            }
+
+            while (word.Count > 0)
+            {
+                Console.Write(word.Pop());
+            }
+        }
+    }
+}
