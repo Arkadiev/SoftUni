6cb122f2849a9ab7632f868f68046b6c8855fe83
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-13 17:28:53 +0200

    Added new exercises

--- 03.Advanced/01.StacksAndQueues/05.PrintEvenNumbers/Program.cs
+++ 03.Advanced/01.StacksAndQueues/05.PrintEvenNumbers/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] input = Console.ReadLine().Split();
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (string number in input)
+            {
+                int currentNumber = int.Parse(number);
+                queue.Enqueue(currentNumber);
+            }
+
+            Queue<int> newQueue = new Queue<int>();
+
+            while (queue.Count > 0)
+            {
+                int number = queue.Dequeue();
+
+                if (number % 2 == 0)
+                {
+                    newQueue.Enqueue(number);
+                }
+            }
+
+            while (newQueue.Count > 1)
+            {
+                Console.Write(newQueue.Dequeue() + ", ");
+            }
+
+            Console.Write(newQueue.Dequeue());
         }
     }
 }
