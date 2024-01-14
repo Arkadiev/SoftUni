6768c61f3072eaeeb0532d62c69b1835f09b4d2e
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-14 13:21:07 +0200

    Added new exercises

---
+++ 03.Advanced/01.StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int n = int.Parse(input[0]);
+            int s = int.Parse(input[1]);
+            int x = int.Parse(input[2]);
+
+            string[] numbers = Console.ReadLine().Split();
+
+            Stack<int> stack = new Stack<int>();
+
+            foreach (string number in numbers)
+            {
+                int currentNumber = int.Parse(number);
+                stack.Push(currentNumber);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            bool isFound = false;
+            int otherNum;
+
+            if (stack.Count == 0)
+            {
+                otherNum = 0;
+            }
+            else
+            {
+                otherNum = int.MaxValue;
+            }
+            
+            while (stack.Count > 0)
+            {
+                int currentNum = stack.Pop();
+                
+                if (currentNum == x)
+                {
+                    isFound = true;
+                }
+                else if (currentNum < otherNum)
+                {
+                    otherNum = currentNum;
+                }
+            }
+
+            if (isFound == true)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(otherNum);
+            }
+        }
+    }
+}
---
+++ 03.Advanced/01.StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _03.MaximumAndMinimumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int command = input[0];
+
+                if (command == 1)
+                {
+                    int number = input[1];
+                    stack.Push(number);
+                }
+                else if (command == 2)
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (command == 3)
+                {
+                    if (stack.Count > 0)
+                    {
+                        list = stack.OrderBy(x => x).ToList();
+                        Console.WriteLine(list[list.Count - 1]);
+                    }
+                }
+                else // 4
+                {
+                    if (stack.Count > 0)
+                    {
+                        list = stack.OrderBy(x => x).ToList();
+                        Console.WriteLine(list[0]);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
