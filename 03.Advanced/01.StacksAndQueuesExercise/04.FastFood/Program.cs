25d67fa2722f15cf4d047c0c61140f80ffcdea31
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-14 18:41:18 +0200

    Added new exercises

---
+++ 03.Advanced/01.StacksAndQueuesExercise/04.FastFood/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(orders);
+            
+            int sum = 0;
+
+            Console.WriteLine(queue.Max());
+
+            while (queue.Count > 0)
+            {
+                int currentNumber = queue.Peek();
+                sum += currentNumber;
+
+                if (sum <= quantity)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    int[] arr = queue.ToArray();
+                    Console.WriteLine($"Orders left: " + string.Join(" ", arr));
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
