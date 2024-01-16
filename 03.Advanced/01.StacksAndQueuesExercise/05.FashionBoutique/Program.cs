173a955cb129dc8bb0da79cf5f51fea4885674ac
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-16 12:40:51 +0200

    Added new exercises

---
+++ 03.Advanced/01.StacksAndQueuesExercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> delivery = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int racksUsed = 1;
+            int currentRackCapacity = rackCapacity;
+
+            while (delivery.Any())
+            {
+                if (delivery.Peek() <= currentRackCapacity)
+                {
+                    currentRackCapacity -= delivery.Pop();
+                }
+                else
+                {
+                    racksUsed++;
+                    currentRackCapacity = rackCapacity;
+                }
+            }
+
+            Console.WriteLine(racksUsed);
+        }
+    }
+}
---
+++ 03.Advanced/01.StacksAndQueuesExercise/06.SongsQueue/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", ").ToArray());
+
+            while (songs.Any())
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                if (input[0] == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (input[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+                else if (input[0] == "Add")
+                {
+                    string songName = string.Join(" ", input.Skip(1));
+
+                    if (songs.Contains(songName))
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(songName);
+                    }
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
