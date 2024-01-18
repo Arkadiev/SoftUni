747ad66e004d1369f9483d64a5ce6258f918a975
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-18 11:47:42 +0200

    Added new exercises

---
+++ 03.Advanced/02.MultidimensionalArrays/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            int colSum = 0;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowArray[col];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    colSum += matrix[row, col];
+                }
+
+                Console.WriteLine(colSum);
+                colSum = 0;
+            }
+        }
+    }
+}
