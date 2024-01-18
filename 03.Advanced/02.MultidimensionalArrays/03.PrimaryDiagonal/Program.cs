260d96a3cb654137bffbe8a2dae19c0ec1f47e83
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-18 12:16:04 +0200

    Added more exercises

---
+++ 03.Advanced/02.MultidimensionalArrays/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int rows = input;
+            int cols = input;
+
+            int diagonalSum = 0;
+
+            int colSum = 0;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowArray[col];
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row == col)
+                    {
+                        diagonalSum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(diagonalSum);
+            return;
+        }
+    }
+}
