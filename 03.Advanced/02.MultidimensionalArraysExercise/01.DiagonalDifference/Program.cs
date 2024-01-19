dedbdd2c2040af2912a177039e3a1c08237dc498
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-19 14:08:42 +0200

    Added new exercises

---
+++ 03.Advanced/02.MultidimensionalArraysExercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _01.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input:
+            int size = int.Parse(Console.ReadLine());
+
+            int sumDiagonalOne = 0;
+            int sumDiagonalTwo = 0;
+
+            int[,] matrix = new int[size, size];
+
+            // Longer Solution:
+            
+            //for (int row = 0; row < size; row++)
+            //{
+            //    int[] newRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //    for (int col = 0; col < size; col++)
+            //    {
+            //        matrix[row, col] = newRow[col];
+
+            //        if (row == col)
+            //        {
+            //            sumDiagonalOne += matrix[row, col];
+            //        }
+            //    }
+            //}
+
+            //for (int row = 0; row < size; row++)
+            //{
+            //    sumDiagonalTwo += matrix[row, size - 1 - row];
+            //}
+
+            // Shorter Solution:
+
+            for (int i = 0; i < size; i++)
+            {
+                int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                sumDiagonalOne += row[i];
+                sumDiagonalTwo += row[size - 1 - i];
+            }
+
+            // Output:
+            Console.WriteLine(Math.Abs(sumDiagonalOne - sumDiagonalTwo));
+        }
+    }
+}
---
+++ 03.Advanced/02.MultidimensionalArraysExercise/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = sizes[0];
+            int cols = sizes[1];
+
+            int squaresFound = 0;
+
+            string[] previousRow = null;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] currentRow = Console.ReadLine().Split();
+
+                if (row > 0)
+                {
+                    for (int col = 0; col < cols - 1; col++)
+                    {
+                        if (currentRow[col] == currentRow[col + 1] &&
+                            currentRow[col] == previousRow[col] &&
+                            currentRow[col] == previousRow[col + 1])
+                        {
+                            squaresFound++;
+                        }
+                    }
+                }
+
+                previousRow = currentRow;
+            }
+
+            Console.WriteLine(squaresFound);
+        }
+    }
+}
