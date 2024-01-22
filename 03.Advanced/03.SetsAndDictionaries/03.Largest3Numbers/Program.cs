583de7c872859ebb4b0bd027efd1558a7f436119
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-22 15:54:01 +0200

    Added new exercises

---
+++ 03.Advanced/03.SetsAndDictionaries/03.Largest3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> largerstThreeNumbers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+
+            Console.WriteLine(string.Join(" ", largerstThreeNumbers));
+        }
+    }
+}
