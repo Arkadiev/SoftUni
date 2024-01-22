152b87b4a6a4751b4071f1144376adf8e8d24b7e
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-22 17:31:33 +0200

    Added new exercises

---
+++ 03.Advanced/03.SetsAndDictionaries/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" \n", names));
+        }
+    }
+}
