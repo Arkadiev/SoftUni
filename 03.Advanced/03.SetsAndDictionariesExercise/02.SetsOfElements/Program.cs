d62b67062456273e375f49afbd8274995ad35054
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-23 14:36:05 +0200

    Added new exercises

---
+++ 03.Advanced/03.SetsAndDictionariesExercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lines = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            HashSet<int> setOne = new HashSet<int>();
+            HashSet<int> setTwo = new HashSet<int>();
+
+            HashSet<int> setThree = new HashSet<int>();
+
+            for (int i = 0; i < lines[0]; i++)
+            {
+                setOne.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < lines[1]; i++)
+            {
+                setTwo.Add(int.Parse(Console.ReadLine()));
+            }
+
+            setOne.IntersectWith(setTwo);
+
+            Console.Write($"{string.Join(" ", setOne)}");
+        }
+    }
+}
