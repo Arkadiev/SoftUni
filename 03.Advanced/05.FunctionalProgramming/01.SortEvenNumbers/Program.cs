68800c0d9d516a21349f2b0b937cd9b1e743b67a
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-30 21:50:04 +0200

    Added new exercises

---
+++ 03.Advanced/05.FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n)).Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+
+            string result = string.Join(", ", numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
