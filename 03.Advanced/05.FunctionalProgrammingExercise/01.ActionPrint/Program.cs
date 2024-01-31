66246516608faee22010db0dfeb4b2a2b06bec0f
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-31 23:20:42 +0200

    Added new exercises

---
+++ 03.Advanced/05.FunctionalProgrammingExercise/01.ActionPrint/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> print = s => Console.WriteLine(s);
+
+            Array.ForEach(input, print);
+        }
+    }
+}
