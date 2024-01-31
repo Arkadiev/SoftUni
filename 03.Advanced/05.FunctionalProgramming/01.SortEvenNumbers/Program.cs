a662d31063aab590176c9426be331a1ad0001dea
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-31 14:00:16 +0200

    Added new exercises

--- 03.Advanced/05.FunctionalProgramming/01.SortEvenNumbers/Program.cs
+++ 03.Advanced/05.FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(n => int.Parse(n)).Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
 
             string result = string.Join(", ", numbers);
             Console.WriteLine(result);
