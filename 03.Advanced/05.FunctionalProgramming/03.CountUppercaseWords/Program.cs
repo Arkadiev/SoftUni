90826aae06341319baf23bf3c52dac484db2bd93
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-01-31 15:27:01 +0200

    Added new exercises

--- 03.Advanced/05.FunctionalProgramming/03.CountUppercaseWords/Program.cs
+++ 03.Advanced/05.FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Predicate<string> checker = word => char.IsUpper(word[0]);
+
+            words = words.Where(w => checker(w)).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
         }
     }
 }
---
+++ 03.Advanced/05.FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).Select(x => x * 1.2m).ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
