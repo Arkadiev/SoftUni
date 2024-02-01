cf329f03ecef4832bd488263caad358e1d533bc4
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-01 12:53:35 +0200

    Added new exercises

---
+++ 03.Advanced/05.FunctionalProgrammingExercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.FindEvensOrOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string criteria = Console.ReadLine();
+
+            List<int> numbers = new();
+
+            for (int i = input[0]; i <= input[1]; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> predicate = n => true;
+
+            if (criteria == "even")
+            {
+                predicate = n => n % 2 == 0;
+            }
+            else if (criteria == "odd")
+            {
+                predicate = n => n % 2 != 0;
+            }
+
+            var filteredNumbers = numbers.Where(new Func<int, bool>(predicate)).ToArray();
+
+            Console.WriteLine(string.Join(" ", filteredNumbers));
+        }
+    }
+}
