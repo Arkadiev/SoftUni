509cd6247902f906e0eec45bf1ee400cc8cf1ddb
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-01 10:00:06 +0200

    Added new exercises

---
+++ 03.Advanced/05.FunctionalProgrammingExercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> promote = s => Console.WriteLine($"Sir {s}");
+
+            Array.ForEach(input, promote);
+        }
+    }
+}
