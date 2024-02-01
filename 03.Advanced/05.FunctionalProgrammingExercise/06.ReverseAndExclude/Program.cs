3e9f66287367971be279cffee2336eebbe2c8ad6
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-01 15:30:01 +0200

    Added new exercises

---
+++ 03.Advanced/05.FunctionalProgrammingExercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06.ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            Predicate<int> predicate = x => x % divisor != 0;
+
+            var result = input.Where(x => predicate(x)).ToArray();
+
+            Console.WriteLine(string.Join(" ", result.Reverse()));
+        }
+    }
+}
