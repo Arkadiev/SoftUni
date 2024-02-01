f0bfe67676603ac5e1cc0dc17dd40f107302e577
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-01 17:03:13 +0200

    Added new exercises

---
+++ 03.Advanced/05.FunctionalProgrammingExercise/06.PredicateForNames/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Predicate<string> predicate = x => x.Length <= length;
+
+            var result = names.Where(name => predicate(name));
+
+            foreach (string name in result)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
