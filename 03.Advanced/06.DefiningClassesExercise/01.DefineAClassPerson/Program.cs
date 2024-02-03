ec7b0070154ae1c85fe81adbdddd4d56993d4eff
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-03 15:57:52 +0200

    Added new exercises

---
+++ 03.Advanced/06.DefiningClassesExercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person Peter = new Person
+            {
+                Name = "Peter",
+                Age = 20
+            };
+
+            Person George = new Person("George", 18);
+
+            Person Jose = new("Jose", 48);
+        }
+    }
+}
