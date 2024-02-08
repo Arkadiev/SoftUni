20e302b0cc5405088c3c6db09ebdc590d8c194ae
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-08 13:35:00 +0200

    Added new exercises

--- 03.Advanced/07.GenericsExercise/08.Threeuple/Program.cs
+++ 03.Advanced/07.GenericsExercise/08.Threeuple/Program.cs
@@ -10,7 +10,17 @@ namespace _08.Threeuple
             string[] beerTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
             string[] bankTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-            Threeuple<string, string, string> names = new($"{nameTokens[0]} {nameTokens[1]}", nameTokens[2], nameTokens[3]);
+            string townName;
+            if (nameTokens.Length == 5)
+            {
+                townName = $"{nameTokens[3]} {nameTokens[4]}";
+            }
+            else
+            {
+                townName = nameTokens[3];
+            }
+
+            Threeuple<string, string, string> names = new($"{nameTokens[0]} {nameTokens[1]}", nameTokens[2], townName);
             Threeuple<string, int, bool> beers = new(beerTokens[0], int.Parse(beerTokens[1]), beerTokens[2] == "drunk");
             Threeuple<string, double, string> banks = new(bankTokens[0], double.Parse(bankTokens[1], CultureInfo.InvariantCulture), bankTokens[2]);
 
