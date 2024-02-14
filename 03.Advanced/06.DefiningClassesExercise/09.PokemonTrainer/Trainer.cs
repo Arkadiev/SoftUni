c98595af9c0c1fa00c79e743ae3d67a9b6c7381d
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-14 21:31:21 +0200

    Added new exercises

---
+++ 03.Advanced/06.DefiningClassesExercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name) => Name = name;
+
+        public string Name { get; set; }
+        public ushort BadgesCount { get; set; }
+        public List<Pokemon> PokemonCollection { get; set; } = new();
+    }
+}
---
+++ 03.Advanced/08.IteratorsAndComparatorsExercise/07.CustomComparator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.CustomComparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            Array.Sort(array, new CustomComparator());
+            Console.WriteLine(string.Join(' ', array));
+        }
+
+        public class CustomComparator : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                if (x % 2 == 0 && y % 2 != 0) return -1;
+                if (x % 2 != 0 && y % 2 == 0) return 1;
+                return x < y ? -1 : 1;
+            }
+        }
+    }
+}
