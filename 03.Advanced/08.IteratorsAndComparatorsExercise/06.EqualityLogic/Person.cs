bbafd8c9d94099c7237cda25a5c7971b97d392be
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-14 17:36:27 +0200

    Added new exercises

---
+++ 03.Advanced/08.IteratorsAndComparatorsExercise/06.EqualityLogic/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Name
+        {
+            get => _name;
+            private set => _name = value;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public int CompareTo(Person? other)
+        {
+            int result = Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Age.GetHashCode();
+        }
+
+        public override bool Equals (object obj)
+        {
+            Person other = obj as Person;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Name == other.Name && Age == other.Age;
+        }
+    }
+}
