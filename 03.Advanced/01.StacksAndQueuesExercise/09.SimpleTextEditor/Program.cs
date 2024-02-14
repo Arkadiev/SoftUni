d0d0152748d8b538779d10fb53595980296fe065
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-14 21:05:30 +0200

    Added new exercises

---
+++ 03.Advanced/01.StacksAndQueuesExercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _09.SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> text = new();
+
+            int n = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine()!;
+                if (text.Count == 0)
+                {
+                    if (command[0] == '1') text.Push(command[2..]);
+                    continue;
+                }
+
+                string current = text.Peek();
+
+                if (command[0] == '1')
+                {
+                    text.Push(current + command[2..]);
+                }
+                else if (command[0] == '2')
+                {
+                    text.Push(current[..^int.Parse(command[2..])]);
+                }
+                else if (command[0] == '3')
+                {
+                    Console.WriteLine(current[int.Parse(command[2..]) - 1]);
+                }
+                else if (command[0] == '4')
+                {
+                    text.Pop();
+                }
+            }
+        }
+    }
+}
