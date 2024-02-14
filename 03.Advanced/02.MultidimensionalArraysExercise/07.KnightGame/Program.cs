42f5fe37cb41f1869b0b3554e6b35086475b1a26
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-14 21:14:16 +0200

    Added new exercises

---
+++ 03.Advanced/02.MultidimensionalArraysExercise/07.KnightGame/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _07.KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte squareMatrixSize = byte.Parse(Console.ReadLine()!);
+            char[,] squareMatrix = new char[squareMatrixSize, squareMatrixSize];
+
+            for (byte i = 0; i < squareMatrixSize; i++)
+            {
+                char[] row = Console.ReadLine()!.ToCharArray();
+                for (byte j = 0; j < squareMatrixSize; j++) squareMatrix[i, j] = row[j];
+            }
+
+            ushort knightsRemoved = 0;
+            for (int max = 8; max > 0; max--)
+            {
+                for (int i = 0; i < squareMatrixSize; i++)
+                {
+                    for (int j = 0; j < squareMatrixSize; j++)
+                    {
+                        byte endangered = 0;
+                        if (squareMatrix[i, j] == 'K')
+                        {
+                            string[] moves =
+                            {
+                                $"{i + 2} {j + 1}", $"{i + 2} {j - 1}", $"{i - 2} {j + 1}", $"{i - 2} {j - 1}",
+                                $"{i + 1} {j + 2}", $"{i + 1} {j - 2}", $"{i - 1} {j + 2}", $"{i - 1} {j - 2}"
+                            };
+
+                            foreach (string move in moves)
+                            {
+                                int y = int.Parse(move.Split()[0]);
+                                int x = int.Parse(move.Split()[1]);
+                                if (y >= 0 && y < squareMatrixSize && x >= 0 && x < squareMatrixSize && squareMatrix[y, x] == 'K') endangered++;
+                            }
+                        }
+
+                        if (endangered == max)
+                        {
+                            squareMatrix[i, j] = '0';
+                            knightsRemoved++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(knightsRemoved);
+        }
+    }
+}
---
+++ 03.Advanced/05.FunctionalProgrammingExercise/11.TriFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minSum = int.Parse(Console.ReadLine()!);
+            string[] people = Console.ReadLine()!.Split();
+
+            Func<string, int, bool> getSum = (name, min) => name.Sum(c => c) >= min;
+            Func<string[], int, Func<string, int, bool>, string> getFirstName = (names, min, checker) => names.First(x => checker(x, min));
+
+            Console.WriteLine(getFirstName(people, minSum, getSum));
+        }
+    }
+}
