4bec3dbb48a3d0509d4ceaced0c85b288cbc845e
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-16 21:25:45 +0200

    Added new exercises

---
+++ 03.Advanced/09.ExamPreparation/06.AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<Vehicle> Vehicles { get; set; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Vehicles.Count < Capacity)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle (string vin)
+        {
+            Vehicle vehicle = Vehicles.Find(v => v.VIN == vin);
+
+            if (vehicle == null)
+            {
+                return false;
+            }
+
+            return Vehicles.Remove(vehicle);
+        }
+
+        public int GetCount() => Vehicles.Count;
+
+        public Vehicle GetLowestMileage()
+        {
+            return Vehicles.MinBy(v => v.Mileage);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Vehicles in the preparatory:");
+
+            foreach (var vehicle in Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
