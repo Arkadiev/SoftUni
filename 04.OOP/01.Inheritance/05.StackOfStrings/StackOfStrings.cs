9caa580f553eec763e07ab9350bcb85dd107d82d
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-24 15:35:04 +0200

    Added new exercises

---
+++ 04.OOP/01.Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => Count == 0;
+
+        public void AddRange(IEnumerable<string> elements)
+        {
+            foreach (string element in elements)
+            {
+                Push(element);
+            }
+        }
+    }
+}
