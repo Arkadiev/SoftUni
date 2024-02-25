243437e549090ddc2fa4603cb1619ffe24f52bc5
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-02-25 17:54:42 +0200

    Added new exercises

---
+++ 04.OOP/01.InheritanceExercise/05.Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double mililiters = 50;
+        private const decimal price = 3.50m;
+
+        public Coffee(string name, double caffeine) : base(name, price, mililiters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
