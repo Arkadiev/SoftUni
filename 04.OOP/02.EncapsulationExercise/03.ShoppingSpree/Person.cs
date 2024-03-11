562b76d0414ba05752487d1ed8cbb6dc16c59004
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-03-11 13:04:11 +0200

    Added more exercises

---
+++ 04.OOP/02.EncapsulationExercise/03.ShoppingSpree/Person.cs
@@ -0,0 +1,57 @@
+﻿namespace _03.ShoppingSpree
+{
+    public class Person
+    {
+		private string name;
+        private int money;
+        private List<Product> bag;
+
+        public Person(string name, int money)
+        {
+            Name = name;
+            Money = money;
+			bag = new List<Product>();
+        }
+
+        public string Name
+		{
+			get => name;
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException($"Name cannot be empty");
+				}
+
+				name = value;
+			}
+		}
+
+		public int Money
+		{
+			get => money;
+			set
+			{
+				if (value < 0)
+				{
+					throw new ArgumentException("Money cannot be negative");
+				}
+
+				money = value;
+			}
+		}
+
+		public List<Product> Bag => bag;
+
+        public void AddProduct(Product product)
+		{
+			if (product.Price > this.Money)
+			{
+				throw new ArgumentException($"{this.Name} can't afford {product.Name}");
+			}
+
+			bag.Add(product);
+			this.Money -= product.Price;
+		}
+	}
+}
---
+++ 04.OOP/02.EncapsulationExercise/03.ShoppingSpree/Product.cs
@@ -0,0 +1,43 @@
+﻿namespace _03.ShoppingSpree
+{
+    public class Product
+    {
+		private string name;
+        private int price;
+
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name
+		{
+			get => name;
+			set
+			{
+				if(string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("Name cannot be empty");
+				}
+
+				name = value;
+			}
+		}
+
+		public int Price
+		{
+			get => price;
+			set
+			{
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                price = value;
+			}
+		}
+
+	}
+}
