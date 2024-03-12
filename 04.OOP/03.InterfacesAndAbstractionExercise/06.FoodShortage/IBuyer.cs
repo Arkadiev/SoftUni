ab9daf9f4e35907e853a49b225bf77207d93b1e3
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-03-12 17:32:54 +0200

    Added new exercises

---
+++ 04.OOP/03.InterfacesAndAbstractionExercise/06.FoodShortage/IBuyer.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodShortage
+{
+    public interface IBuyer
+    {
+        public string Name { get; set; }
+
+        public int Food { get; set; }
+
+        public void BuyFood();
+    }
+}
---
+++ 04.OOP/03.InterfacesAndAbstractionExercise/06.FoodShortage/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace FoodShortage
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Citizen> citizens = new List<Citizen>();
+            List<Rebel> rebels = new List<Rebel>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input.Length == 4) // Citizen
+                {
+                    string name = input[0];
+                    int age = int.Parse(input[1]);
+                    string id = input[2];
+                    string birthdate = input[3];
+
+                    Citizen citizen = new Citizen(name, age, id, birthdate);
+                    citizens.Add(citizen);
+                }
+                else if (input.Length == 3) // Rebel
+                {
+                    string name = input[0];
+                    int age = int.Parse(input[1]);
+                    string group = input[2];
+
+                    Rebel rebel = new Rebel(name, age, group);
+                    rebels.Add(rebel);
+                }
+            }
+
+            int foodPurchased = 0;
+
+            string input2;
+            while ((input2 = Console.ReadLine()) != "End")
+            {
+                if (citizens.Any(c => c.Name == input2))
+                {
+                    foodPurchased += 10;
+                }
+                else if (rebels.Any(r => r.Name == input2))
+                {
+                    foodPurchased += 5;
+                }
+            }
+
+            Console.WriteLine(foodPurchased);
+        }
+    }
+}
