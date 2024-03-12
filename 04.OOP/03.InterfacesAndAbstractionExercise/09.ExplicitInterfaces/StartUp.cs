73a94c74db383aad6b3a621277dc0f5801635328
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-03-12 18:21:32 +0200

    Added new exercises

---
+++ 04.OOP/03.InterfacesAndAbstractionExercise/09.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var tokens = input.Split();
+
+                Citizen citizen = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
+        }
+    }
+}
