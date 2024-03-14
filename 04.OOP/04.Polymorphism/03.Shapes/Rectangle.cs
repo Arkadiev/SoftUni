f66a7ad1bd5b427b34881595e24259b8ccc050c9
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-03-14 12:42:53 +0200

    Added new exercises

---
+++ 04.OOP/04.Polymorphism/03.Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private readonly double height;
+        private readonly double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return height * width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (height + width) * 2;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + nameof(Rectangle);
+        }
+    }
+}
