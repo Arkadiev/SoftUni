adf7f1b202b6be85bb8ca2ed745403d00cd79a9b
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-03-16 15:46:20 +0200

    Added new exercises

---
+++ 04.OOP/04.PolymorphismExercise/03.Raiding/BaseHero.cs
@@ -0,0 +1,17 @@
+﻿namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        protected string Name;
+
+        protected BaseHero(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+
+        public int Power { get; protected set; }
+
+        public abstract string CastAbility();
+    }
+}
---
+++ 04.OOP/04.PolymorphismExercise/03.Raiding/StartUp.cs
@@ -0,0 +1,68 @@
+﻿namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<BaseHero> party = new List<BaseHero>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string heroName = Console.ReadLine();
+                string heroClass = Console.ReadLine();
+
+                if (heroClass == "Druid")
+                {
+                    Druid druid = new Druid(heroName);
+                    party.Add(druid);
+                }
+                else if (heroClass == "Paladin")
+                {
+                    Paladin paladin = new Paladin(heroName);
+                    party.Add(paladin);
+                }
+                else if (heroClass == "Rogue")
+                {
+                    Rogue rogue = new Rogue(heroName);
+                    party.Add(rogue);
+                }
+                else if (heroClass == "Warrior")
+                {
+                    Warrior warrior = new Warrior(heroName);
+                    party.Add(warrior);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid hero!");
+                    i--;
+                }
+            }
+
+            int bossPower = int.Parse(Console.ReadLine());
+            int partyPower = 0;
+
+            foreach (BaseHero hero in party)
+            {
+                Console.WriteLine(hero.CastAbility());
+
+                if (hero.GetType().Name == "Druid" ||  hero.GetType().Name == "Rogue")
+                {
+                    partyPower += 80;
+                }
+                else if (hero.GetType().Name == "Paladin" || hero.GetType().Name == "Warrior")
+                {
+                    partyPower += 100;
+                }
+
+                if (partyPower >= bossPower)
+                {
+                    Console.WriteLine("Victory!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Defeat...");
+        }
+    }
+}
