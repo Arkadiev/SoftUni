72d1c52323383bedaa4aaeffdc55d4ac524cdbb7
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-03-23 17:37:40 +0200

    Added new exercises

---
+++ 04.OOP/06.ReflectionAndAttributesExercise/CommandPattern/Interpreters/CommandInterpreter.cs
@@ -0,0 +1,29 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Interpreters
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] parts = args.Split();
+
+            string command = $"{parts[0]}Command";
+            string[] commandArgs = parts.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Type type = assembly?.GetTypes().FirstOrDefault(t => t.Name == command);
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid type!");
+            }
+
+            var instance = (ICommand)Activator.CreateInstance(type);
+            return instance?.Execute(commandArgs);
+        }
+    }
+}
