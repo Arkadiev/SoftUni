42c909c6853ca77bbdb1c96a3f3431f719aa43e5
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-07-02 18:22:05 +0300

    Added new exercises

---
+++ 06.EntityFrameworkCore/03.LINQ/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-M3E5HH3\SQLEXPRESS; Database=MusicHub; Integrated Security=True";
+    }
+}
