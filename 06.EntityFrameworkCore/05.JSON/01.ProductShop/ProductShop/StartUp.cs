cc6cb6cc717bf740bc73216eb1d61c47830b1752
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-07-22 15:22:15 +0300

    Added new exercises

---
+++ 06.EntityFrameworkCore/05.JSON/01.ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,202 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            // 01 - Import Users
+            //string usersText = File.ReadAllText("../../../Datasets/users.json");
+            //Console.WriteLine(ImportUsers(context, usersText));
+
+            // 02 - Import Products
+            //string productsText = File.ReadAllText("../../../Datasets/products.json");
+            //Console.WriteLine(ImportProducts(context, productsText));
+
+            // 03 - Import Categories
+            //string categoriesText = File.ReadAllText("../../../Datasets/categories.json");
+            //Console.WriteLine(ImportProducts(context, categoriesText));
+
+            // 04 - Import Categories and Products
+            //string categoriesProductsText = File.ReadAllText("../../../Datasets/categories-products.json");
+            //Console.WriteLine(ImportCategoryProducts(context, categoriesProductsText));
+
+            // 05 - Export Products In Range
+            //Console.WriteLine(GetProductsInRange(context));
+
+            // 06 - Export Sold Products
+            //Console.WriteLine(GetSoldProducts(context));
+
+            // 07 - Export Categories By Product Count
+            //Console.WriteLine(GetCategoriesByProductsCount(context));
+
+            // 08 - Export Users and Products
+            //Console.WriteLine(GetUsersWithProducts(context));
+        }
+
+        // 01 - Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        // 02 - Import Products
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+
+        // 03 - Import Categories
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<List<Category>>(inputJson);
+
+            categories.RemoveAll(c => c.Name == null);
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
+        }
+
+        // 04 - Import Categories and Products
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoriesProducts = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+
+            context.CategoriesProducts.AddRange(categoriesProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProducts.Count}";
+        }
+
+        // 05 - Export Products In Range
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var productsInRange = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .Select(p => new ExportProductDto()
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    Seller = $"{p.Seller.FirstName} {p.Seller.LastName}"
+                })
+                .ToList();
+
+            return SerializeObjectWithJsonSettings(productsInRange);
+        }
+
+        // 06 - Export Sold Products
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var soldProducts = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.BuyerId != null))
+                .Select(u => new SellerWithProductsDto()
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    SoldProducts = u.ProductsSold.Select(p => new SoldProductsDto()
+                    {
+                        Name = p.Name,
+                        Price = p.Price,
+                        BuyerFirstName = p.Buyer.FirstName,
+                        BuyerLastName = p.Buyer.LastName
+                    })
+                })
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
+            return SerializeObjectWithJsonSettings(soldProducts);
+        }
+
+        // 07 - Export Categories By Product Count
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories = context.Categories
+                .Select(c => new
+                {
+                    Category = c.Name,
+                    ProductsCount = c.CategoriesProducts.Count,
+                    AveragePrice = c.CategoriesProducts.Average(cp => cp.Product.Price).ToString("f2"),
+                    TotalRevenue = c.CategoriesProducts.Sum(cp => cp.Product.Price).ToString("f2")
+                })
+                .OrderByDescending(x => x.ProductsCount)
+                .ToList();
+
+            return SerializeObjectWithJsonSettings(categories);
+        }
+
+        // 08 - Export Users and Products
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.BuyerId != null && p.Price != null))
+                .Select(u => new
+                {
+                    u.FirstName,
+                    u.LastName,
+                    u.Age,
+                    SoldProducts = u.ProductsSold
+                        .Where(p => p.BuyerId != null && p.Price != null)
+                        .Select(p => new
+                        {
+                            p.Name,
+                            p.Price
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(u => u.SoldProducts.Length)
+                .ToArray();
+
+            var output = new
+            {
+                UsersCount = users.Length,
+                Users = users.Select(u => new
+                {
+                    u.FirstName,
+                    u.LastName,
+                    u.Age,
+                    SoldProducts = new
+                    {
+                        Count = u.SoldProducts.Length,
+                        Products = u.SoldProducts
+                    }
+                })
+            };
+
+            return SerializeObjectWithJsonSettings(output);
+        }
+
+        private static string SerializeObjectWithJsonSettings(object obj)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
---
+++ 06.EntityFrameworkCore/05.JSON/02.CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // 09 - Import Suppliers
+
+            // 10 - Import Parts
+
+            // 11 - Import Cars
+
+            // 12 - Import Customers
+
+            // 13 - Import Sales
+
+            // 14 - Export Ordered Customers
+
+            // 15 - Export Cars From Make Toyota
+
+            // 16 - Export Local Suppliers
+
+            // 17 - Export Cars With Their List Of Parts
+
+            // 18 - Export Total Sales By Customer
+
+            // 19 - Export Sales With Applied Discount
+        }
+
+        // 09 - Import Suppliers
+
+        // 10 - Import Parts
+
+        // 11 - Import Cars
+
+        // 12 - Import Customers
+
+        // 13 - Import Sales
+
+        // 14 - Export Ordered Customers
+
+        // 15 - Export Cars From Make Toyota
+
+        // 16 - Export Local Suppliers
+
+        // 17 - Export Cars With Their List Of Parts
+
+        // 18 - Export Total Sales By Customer
+
+        // 19 - Export Sales With Applied Discount
+    }
+}
