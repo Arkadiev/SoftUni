acfef6f69fd91c28990bac204a8256076dd24e34
Arkadiev <145834850+Arkadiev@users.noreply.github.com>
2024-09-24 22:27:50 +0300

    Added new exercises

---
+++ 08.ASP.NET-Fundamentals/01.Introduction/CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using CinemaApp.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            ViewData["Message"] = "Welcome to the Cinema Web App";
+
+            return View();
+        }
+    }
+}
