0188d1d7a2006f4930d3a96101ff5daab8593dcb
Arkadiev <georgi.arkadiev.96@gmail.com>
2024-10-03 16:32:23 +0300

    Added new exercises

--- 08.ASP.NET-Fundamentals/01.Introduction/CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/CinemaController.cs
+++ 08.ASP.NET-Fundamentals/01.Introduction/CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/CinemaController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CinemaApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Cinema = CinemaApp.Data.Models.Cinema;
 
 namespace CinemaApp.Web.Controllers
 {
     public class CinemaController : Controller
     {
-        
+        private readonly CinemaDbContext dbContext;
+
+        public CinemaController(CinemaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+			IEnumerable<Cinema> cinemas = this.dbContext.Cinemas.ToArray();
+        }
     }
 }
