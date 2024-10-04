af16e93b5975a054eb44eb2fb5ffbe33dfc1fda8
Arkadiev <145834850+Arkadiev@users.noreply.github.com>
2024-10-04 13:39:31 +0300

    Added new exercises

--- 08.ASP.NET-Fundamentals/01.Introduction/CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/CinemaController.cs
+++ 08.ASP.NET-Fundamentals/01.Introduction/CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/CinemaController.cs
@@ -1,6 +1,8 @@
 ﻿using CinemaApp.Data;
+using CinemaApp.Data.Models;
+using CinemaApp.Web.ViewModels.Cinema;
 using Microsoft.AspNetCore.Mvc;
-using Cinema = CinemaApp.Data.Models.Cinema;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaApp.Web.Controllers
 {
@@ -13,9 +15,89 @@ namespace CinemaApp.Web.Controllers
             this.dbContext = dbContext;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-			IEnumerable<Cinema> cinemas = this.dbContext.Cinemas.ToArray();
+			IEnumerable<CinemaIndexViewModel> cinemas = await this.dbContext
+                .Cinemas
+                .Select(c => new CinemaIndexViewModel()
+                {
+                    Id = c.Id.ToString(),
+                    Name = c.Name,
+                    Location = c.Location
+                })
+                .OrderBy(c => c.Location)
+                .ToArrayAsync();
+
+            return this.View(cinemas);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddCinemaFormModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            Cinema cinema = new Cinema()
+            {
+                Name = model.Name,
+                Location = model.Location
+            };
+
+            await this.dbContext.Cinemas.AddAsync(cinema);
+            await this.dbContext.SaveChangesAsync();
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string? id)
+        {
+			Guid cinemaGuid = Guid.Empty;
+
+			bool isIdValid = IsCinemaIdValid(id, ref cinemaGuid);
+            if (!isIdValid)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+			Cinema? cinema = await this.dbContext
+				.Cinemas
+				.FirstOrDefaultAsync(c => c.Id == cinemaGuid);
+
+			// Invalid (non-existing) GUID in the URL
+			if (cinema == null)
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
+
+
+		}
+
+        private bool IsCinemaIdValid(string? id, ref Guid cinemaGuid)
+        {
+			// Non-existing parameter in the URL
+			if (String.IsNullOrWhiteSpace(id))
+			{
+				return false;
+			}
+
+			// Invalid parameter in the URL
+			bool isGuidValid = Guid.TryParse(id, out cinemaGuid);
+			if (!isGuidValid)
+			{
+				return false;
+			}
+
+            return true;
+		}
     }
 }
